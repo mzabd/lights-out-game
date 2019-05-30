74de592ae60ee584910c24b4479f531bf7ab9fe8
Mohammad Alam <mzabd162@gmail.com>
2019-05-30 11:11:47 -0400

    Added the display board for game

--- src/Board.js
+++ src/Board.js
@@ -30,11 +30,20 @@ import './Board.css';
  **/
 
 class Board extends Component {
-
+  static defaultProps = {
+    nRows: 5,
+    nCols: 5,
+    chanceLightStartsOn: 0.25 //this is a percentile value in float
+  }
   constructor(props) {
     super(props);
 
-    // TODO: set initial state
+    //set initial state
+    this.state = {
+      hasWon: false, 
+      board: this.createBoard()
+    }
+
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -42,6 +51,17 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for(let x = 0; x <this.props.nRows; x++){
+      let row = []; //for each single row
+      //fill up the each row
+      for(let y = 0; y < this.props.nCols; y++){    
+      //set light is on or off, we use math.random which ranges from 0 - 1 to compare our chanceLight porps (.25
+      //if the chances are less than 25% the lights are on i.e true and we push the boolean to row as true
+        row.push(Math.random() < this.props.chanceLightStartsOn);
+      }
+      //now insert the all rows to board arrya
+      board.push(row);
+    }
     return board
   }
 
@@ -50,9 +70,10 @@ class Board extends Component {
   flipCellsAround(coord) {
     let {ncols, nrows} = this.props;
     let board = this.state.board;
+    //
     let [y, x] = coord.split("-").map(Number);
 
-
+    //a nested function - helper
     function flipCell(y, x) {
       // if this coord is actually on board, flip it
 
@@ -66,14 +87,38 @@ class Board extends Component {
     // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    this.setState({board, hasWon});
+    //this.setState({board, hasWon});
   }
 
 
   /** Render game board or winning message. */
-
+  
   render() {
+    //if the game is own just show winning mesage
+    //todo
+    //make the tableBoard which is basically an array: we follow the same logic we used to createBoard
+    let tblBoard = [];
+    //
+    for(let x =0; x < this.props.nRows; x++){
+      let row = [];
+      for(let y = 0; y < this.props.nCols; y++){
+        //now push each Cell component into the row array whith props for Cell islit and the value come
+        //from state board x, y value as we alredy set it thru creatBoard()
+        //also set key with x y index value
+        let coord = `${x}-${y}`;
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]}/>)
+      }
+      //we need to push row using tr as per the html table structure
+      tblBoard.push(<tr>{row}</tr>);
+    }
 
+    return(
+      <table className="Board">
+        <tbody>
+          {tblBoard}
+        </tbody>
+      </table>
+    )
     // if the game is won, just show a winning msg & render nothing else
 
     // TODO
